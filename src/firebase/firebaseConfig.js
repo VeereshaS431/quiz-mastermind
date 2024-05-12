521e9adeae53d4405b41493bb2b55fb471df0690
VeereshaS431 <veereshveerus2257@gmail.com>
2024-05-12 15:49:28 +0530

    added to table

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCbpHqvWpi2dY7_7Im8_axels2-BvxLTFY",
+  authDomain: "fir-d3809.firebaseapp.com",
+  databaseURL: "https://fir-d3809-default-rtdb.firebaseio.com",
+  projectId: "fir-d3809",
+  storageBucket: "fir-d3809.appspot.com",
+  messagingSenderId: "963338611697",
+  appId: "1:963338611697:web:43e791729efa7becb7b250"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ src/routers/navigation-stack.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom"
+import MiniDrawer from "../admin/dashbord"
+import HtmlPage from "../admin/html-page"
+import CssPage from "../admin/css-page"
+import JavaScriptPage from "../admin/javascript-page"
+import { createContext } from "react"
+import HtmlTablePage from "../admin/html-table-page"
+
+export const DataShare=createContext()
+export const Navigation=()=>{
+    return(
+        <DataShare.Provider value={{
+            
+        }}>
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" Component={MiniDrawer}/>
+            <Route path="/html" Component={HtmlPage}/>
+            <Route path="/css" Component={CssPage}/>
+            <Route path="/javascript" Component={JavaScriptPage}/>
+            <Route path="/htmltable" Component={HtmlTablePage}/>
+        </Routes>
+        </BrowserRouter>
+        </DataShare.Provider>
+    )
+}
